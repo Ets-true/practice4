aac07c2cfc138501744d7bcea2c8fe65095bd45e
Denis Balenko <golovoyponik@gmail.com>
2020-11-12 01:08:35 +0300

    ---

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square();
+        System.out.println(circle);
+        System.out.println(rectangle);
+        System.out.println(square);
+    }
+
+}
---
+++ src/com/company/Square.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Square extends Rectangle {
+
+    public Square() {
+        super();
+        this.length = 0;
+        this.width = 0;
+    }
+
+    public Square(double side) {
+        super();
+        this.width = side;
+        this.length = side;
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(color, filled);
+        this.width = side;
+        this.length = side;
+    }
+
+    public double getSide() {
+        return width;
+    }
+
+    public void setSide(double side) {
+        this.width = side;
+        this.length = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
